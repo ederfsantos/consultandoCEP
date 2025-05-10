56388ab7d84625ce0034bcff3995853542e25d56
Eder Ferreira Santos <ederfsantos@live.com>
2025-05-10 17:45:24 -0300

    criando a classe CEP um record

---
+++ src/modelo/Cep.java
@@ -0,0 +1,9 @@
+package modelo;
+
+public record Cep(String logradouro, String complemento, String unidade, String bairro,
+                  String localidade, String uf, String estado, String regiao,
+                  String ibge, String gia, String ddd, String siafi) {
+}
+
+
+
